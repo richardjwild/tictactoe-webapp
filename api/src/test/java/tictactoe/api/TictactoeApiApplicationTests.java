8a45fabd26306760258cf87bcbc548c88e98065d
Richard Wild <richardwild76@gmail.com>
2022-07-20 16:46:58 +0100

    Add generated Spring Boot application

---
+++ api/src/test/java/tictactoe/api/TictactoeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tictactoe.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
