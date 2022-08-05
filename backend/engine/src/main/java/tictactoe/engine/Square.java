57a0b1cab3e2dda973f7f4b83c8276e73d8acf1f
Richard Wild <richardwild76@gmail.com>
2022-08-05 09:14:18 +0100

    Refactor Square to use Stream

--- backend/engine/src/main/java/tictactoe/engine/Square.java
+++ backend/engine/src/main/java/tictactoe/engine/Square.java
@@ -1,13 +1,15 @@
 package tictactoe.engine;
 
+import java.util.Arrays;
+
 public enum Square {
     TOP_MIDDLE, TOP_RIGHT, CENTRE_LEFT, CENTRE_MIDDLE, BOTTOM_LEFT, CENTRE_RIGHT, BOTTOM_MIDDLE, BOTTOM_RIGHT, TOP_LEFT;
 
     public static Square fromString(String string) {
-        for (Square value : Square.values()) {
-            if (value.name().equals(string))
-                return value;
-        }
-        throw new IllegalArgumentException("Unknown value for square");
+        return Arrays.stream(Square.values())
+                .filter(square -> square.name().equals(string))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Unknown value for square"));
     }
+
 }
