af47294635a85aaaacedf7afb7113317c4c89ba7
Richard Wild <rich@Richards-MBP.fritz.box>
2023-05-14 21:26:17 +0100

    Rename state variable

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -4,10 +4,10 @@ import Board from './Board';
 
 export default function Game() {
 
-    const [ state, setState ] = useState(null);
+    const [ gameState, setGameState ] = useState(null);
 
     useEffect(() => {
-        if (!state) {
+        if (!gameState) {
             doNewGame();
         }
     });
@@ -23,7 +23,7 @@ export default function Game() {
     async function play(square) {
         let requestBody = new FormData()
         requestBody.set("square", square)
-        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/games/${state.gameId}/play`, {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/games/${gameState.gameId}/play`, {
             method: 'POST',
             body: requestBody,
         })
@@ -34,23 +34,23 @@ export default function Game() {
     }
 
     function doNewGame() {
-        newGame().then(data => setState(data));
+        newGame().then(initialState => setGameState(initialState));
     }
 
     function doPlay(square) {
-        play(square).then(data => setState(data))
+        play(square).then(newState => setGameState(newState))
     }
 
     return (
         <div className="game">
             <div className="game-board">
                 <Board
-                    squares={state?.board}
+                    squares={gameState?.board}
                     onClick={doPlay}
                 />
             </div>
             <div className="game-info">
-                <p>Status: {state?.status}, to play: {state?.nextUp}</p>
+                <p>Status: {gameState?.status}, to play: {gameState?.nextUp}</p>
             </div>
             <div className="new-game">
                 <button onClick={doNewGame}>New game</button>
